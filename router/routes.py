de87e508f0b668bb52ba9d52177914202f79074a
Kevin Ramirez <kevinr.ingeniero@gmail.com>
2023-05-06 12:06:30 -0500

    encadenando peticion post

--- router/routes.py
+++ router/routes.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
+from schema.user_schema import UserSchema
 
 route = APIRouter()
 
 @route.get("/")
 def root():
     return {"message": "Usted se encuentra en la raíz."}
+
+@route.post("/api/user")
+def create_user(user_data:UserSchema):
+    print(user_data)
